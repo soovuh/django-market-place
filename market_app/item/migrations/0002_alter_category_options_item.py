bb0e1edb3827949b85d221f1ed4305ac42203178
Klymenko Illia <sovuh2703@gmail.com>
2023-04-26 19:08:41 +0300

    Created adding items logic and fix some front bugs

--- market_app/item/migrations/0002_alter_category_options_item.py
+++ market_app/item/migrations/0002_alter_category_options_item.py
@@ -25,7 +25,7 @@ class Migration(migrations.Migration):
                 ('description', models.TextField(blank=True, null=True)),
                 ('price', models.FloatField()),
                 ('image', models.ImageField(blank=True, null=True, upload_to='item_images')),
-                ('is_sold', models.BooleanField(default=True)),
+                ('is_sold', models.BooleanField(default=False)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='item.category')),
                 ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to=settings.AUTH_USER_MODEL)),
