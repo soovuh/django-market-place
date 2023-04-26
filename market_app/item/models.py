0d446158e2a20027927c0d03cd38efa0fc91dfe6
Klymenko Illia <sovuh2703@gmail.com>
2023-04-26 19:24:34 +0300

    Fix bug

--- market_app/item/models.py
+++ market_app/item/models.py
@@ -17,7 +17,7 @@ class Item(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
-    is_sold = models.BooleanField(default=True)
+    is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     
