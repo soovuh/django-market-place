cda6dec24baca0e00475dc9f95dfbeac2f97212d
Klymenko Illia <sovuh2703@gmail.com>
2023-04-27 18:25:22 +0300

    Created search and filter system

--- market_app/item/urls.py
+++ market_app/item/urls.py
@@ -6,8 +6,9 @@ app_name = 'item'
 
 
 urlpatterns = [
+    path('', views.browse, name='browse'),
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
-     path('<int:pk>/edit/', views.edit, name='edit'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
 ]
